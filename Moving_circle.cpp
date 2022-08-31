17309a73f67001064284461bbf9768da9675532b
vineet <vineet@vrikster.com>
2022-08-30 23:02:59 -0700

    Made auto moving rectangle

--- Moving_circle.cpp
+++ Moving_circle.cpp
@@ -7,6 +7,12 @@ int main(){
     SetTargetFPS(60);
     int circle_x = 300;
     int circle_y = 300;
+
+    int rectangle_x = 600;
+    int rectangle_y = 800;
+
+    int driection = 4;
+
     while (!WindowShouldClose())    // or  WindowShouldClose() == false , this function will close a window when an esc key or windows is closed 
     {
         BeginDrawing();
@@ -15,18 +21,27 @@ int main(){
         //Game logic begins
         
         DrawCircle(circle_x,circle_y,40,BLUE);
+        DrawRectangle(rectangle_x, rectangle_y, 50, 50, RED);         //DrwawRectangle(distance_x,distance_y,width,height,color)
         
-        if(IsKeyDown(KEY_D)){
-            circle_x = circle_x + 2;
+        
+        //move the rectangle
+        rectangle_y += driection;
+        if(rectangle_y > 800 || rectangle_y < 0){   // This function basically changes the +ve to -ve and -ve to +ve after eveytime it reaches the 800 or 0, so direction becomes (-4) and after it reaches top it becomes (+4) and this is how the box keeps moving
+            driection = -driection;
         }
-        if(IsKeyDown(KEY_A)){
-            circle_x = circle_x - 2;
+
+        //controlling the circle
+        if(IsKeyDown(KEY_D) && circle_x <= 800){
+            circle_x += 10;
         }
-        if(IsKeyDown(KEY_S)){
-            circle_y = circle_y + 2;
+        if(IsKeyDown(KEY_A) && circle_x >= 0){
+            circle_x -= 10;
         }
-        if(IsKeyDown(KEY_W)){
-            circle_y = circle_y - 2;
+        if(IsKeyDown(KEY_S) && circle_y <= 800){
+            circle_y += 10;
+        }
+        if(IsKeyDown(KEY_W) && circle_y >= 0){
+            circle_y -= 10;
         }
 
         // if(circle_x >= 800 || circle_y >= 800){
