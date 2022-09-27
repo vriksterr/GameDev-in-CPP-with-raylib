49f38bccdd0ca9373ff15fb1adadb4acfde5f632
vineet <vineet@vrikster.com>
2022-09-27 22:36:20 +0530

    Added boundaries for future collision detection

--- Moving_circle.cpp
+++ Moving_circle.cpp
@@ -6,8 +6,8 @@ int main(){
     InitWindow(Window_Width,Window_Height,"Circle Moving Game by.Vrikster");
 
     SetTargetFPS(60);
-    int circle_x = 300;
-    int circle_y = 300;
+    int circle_x_position = 300;
+    int circle_y_position = 300;
     int circle_radius = 40;
     int circle_increment = 5;
 
@@ -17,11 +17,21 @@ int main(){
     int rectangle_width = 50;
     int rectangle_height = 300;
     
-    
-    
+    //Circle Boundries
+    int circle_top_edge = circle_y_position - circle_radius;
+    int circle_bottom_edge = circle_y_position + circle_radius;
+    int circle_left_edge = circle_x_position - circle_radius;
+    int circle_right_edge = circle_x_position + circle_radius;
+
+    //Rectangle Boundries
+    int rectangle_top_edge = rectangle_y - rectangle_height;
+    int rectangle_bottom_edge = rectangle_y + rectangle_height;
+    int rectangle_left_edge = rectangle_x - rectangle_width;
+    int rectangle_right_edge = rectangle_x + rectangle_width;
 
     int direction = 4;
 
+    bool collision_with_axe {false};
 
     // once the while loops starts the program keeps looping within while loop until the game is closed
 
@@ -30,39 +40,45 @@ int main(){
         BeginDrawing();
         ClearBackground(WHITE);
 
-        //Game logic begins
         
-        DrawCircle(circle_x,circle_y,circle_radius,BLUE);
-        DrawRectangle(rectangle_x, rectangle_y, rectangle_width, rectangle_height, RED);         //DrwawRectangle(distance_x,distance_y,width,height,color)
-        
-        
-        //move the rectangle
-        rectangle_y += direction;
-        //rectangle edges
-        int rectangle_edge_top = rectangle_y;
-        int rectangle_edge_bottom = rectangle_y + rectangle_height;
-        // int rectangle_edge_left = rectangle_x;
-        // int rectangle_edge_right = rectangle_y + rectangle_width;
-        
-        if( rectangle_edge_bottom >= Window_Height || rectangle_edge_top <= 0){   // This function basically changes the +ve to -ve and -ve to +ve after eveytime it reaches the 800 or 0, so direction becomes (-4) and after it reaches top it becomes (+4) and this is how the box keeps moving
-            direction = -direction;
+        if(collision_with_axe){
+            DrawText("Game Over!", 400, 200, 20, RED);
+        }
+        else{
+            //Game logic begins
+            DrawCircle(circle_x_position,circle_y_position,circle_radius,BLUE);
+            DrawRectangle(rectangle_x, rectangle_y, rectangle_width, rectangle_height, RED);         //DrwawRectangle(distance_x,distance_y,width,height,color)
+            
+            
+            //move the rectangle
+            rectangle_y += direction;
+            //rectangle edges
+            int rectangle_edge_top = rectangle_y;
+            int rectangle_edge_bottom = rectangle_y + rectangle_height;
+            // int rectangle_edge_left = rectangle_x;
+            // int rectangle_edge_right = rectangle_y + rectangle_width;
+            
+            if( rectangle_edge_bottom >= Window_Height || rectangle_edge_top <= 0){   // This function basically changes the +ve to -ve and -ve to +ve after eveytime it reaches the 800 or 0, so direction becomes (-4) and after it reaches top it becomes (+4) and this is how the box keeps moving
+                direction = -direction;
+            }
+
+            //controlling the circle position
+            if(IsKeyDown(KEY_D) && circle_x_position+circle_radius <= 800){
+                circle_x_position += circle_increment;
+            }
+            if(IsKeyDown(KEY_A) && circle_x_position-circle_radius >= 0){
+                circle_x_position -= circle_increment;
+            }
+            if(IsKeyDown(KEY_S) && circle_y_position+circle_radius <= 800){
+                circle_y_position += circle_increment;
+            }
+            if(IsKeyDown(KEY_W) && circle_y_position-circle_radius >= 0){
+                circle_y_position -= circle_increment;
+            }
+
+            //Game logic ends
         }
 
-        //controlling the circle
-        if(IsKeyDown(KEY_D) && circle_x+circle_radius <= 800){
-            circle_x += circle_increment;
-        }
-        if(IsKeyDown(KEY_A) && circle_x-circle_radius >= 0){
-            circle_x -= circle_increment;
-        }
-        if(IsKeyDown(KEY_S) && circle_y+circle_radius <= 800){
-            circle_y += circle_increment;
-        }
-        if(IsKeyDown(KEY_W) && circle_y-circle_radius >= 0){
-            circle_y -= circle_increment;
-        }
-
-        //Game logic ends
         EndDrawing();
     }
 }
